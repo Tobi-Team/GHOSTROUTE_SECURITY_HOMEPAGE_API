ef17a752dca2b7f14b2698002c01d0212f8f6235
Ayobami6 <ayobamidele006@gmail.com>
2025-03-01 07:44:22 +0100

    Add initial project structure and configuration files

---
+++ config/db.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, schema
+from sqlalchemy.orm import (
+    sessionmaker,
+    scoped_session,
+    Session,
+)
+from config.env_configs import configs
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Iterator
+
+environment = configs.ENV
+
+engine = (
+    create_engine(configs.TEST_DB_URL)
+    if environment == "test"
+    else create_engine(
+        configs.DB_URL,
+        connect_args={"options": "-c timezone=utc"},
+        pool_size=20,
+        max_overflow=0,
+        pool_timeout=300,
+        pool_recycle=300,
+        pool_pre_ping=True,
+        pool_use_lifo=True,
+        isolation_level="READ COMMITTED",
+    )
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    expire_on_commit=False,
+)
+
+Base = (
+    declarative_base()
+    if environment == "test"
+    else declarative_base(bind=engine, schema=schema.Schema("public"))
+)
+
+Base.query = scoped_session(SessionLocal).query_property()
+
+
+def get_db() -> Iterator[Session]:
+    db: Session = SessionLocal()
+    try:
+        yield db
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        db.close()
---
+++ config/env_configs.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+env = os.getenv("ENV", "test")
+
+
+class AppConfigs(BaseSettings):
+    DB_URL: str = ""
+    TEST_DB_URL: str = ""
+    ENV: str = env
+    SECRET_KEY: str = ""
+    ALGORITHM: str = "HS512"
+    ACCESS_TOKEN_EXPIRES: int = 6
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_PORT: int = 465
+    MAIL_USERNAME: str = ""
+    MAIL_PASSWORD: str = ""
+    MAIL_FROM: str = "default-email@example.com"
+    DEBUG: bool = True if env == "dev" else False
+
+
+configs = AppConfigs()
+
+__all__ = ["configs"]
